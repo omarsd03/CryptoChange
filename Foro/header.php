fa4935d06e9675f635bb617c80e4a9a71ce2eb1f
omarsd03 <34853959+omarsd03@users.noreply.github.com>
2018-05-13 18:29:07 -0500

    Segundo Cambio del Foro

--- Foro/header.php
+++ Foro/header.php
@@ -1,56 +1,51 @@
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <link href='../images/logo/logo.png' rel='shortcut icon' type='image/png'>
-        <title>FORO | CRYPTO CHANGE</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width">
-        <link rel="stylesheet" href="css/estilo.css" />
-        <link rel="stylesheet" href="css/bootstrap.css">
+<head>
+	<meta charset="UTF-8">
+	<link href='images/logo/logo.png' rel='shortcut icon' type='image/png'>
+	<title>Crypto Change</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/estilo.css" />
+	<link rel="stylesheet" href="icomoon/style.css">
+</head>
+<body>
 
-    </head>
-    <body>
-        <div id="CajaPrincipal">
-            <div class="cajaHeader">
-                <div class="cajaBuscar">
-                    <form action='buscar.php' method='post'>
-                        <input type='text' name='busqueda' required="required">
-                        <button type="submit" class="btn btn-primary">
+<header>
+    <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="#" class="navbar-brand">
+                </a>
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navegacion-fm">
+                    <span class="sr-only"> Desplegar / Ocultar Menu</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+
+            <!-- Inicia Menu -->
+
+            <div class="collapse navbar-collapse" id="navegacion-fm">
+                <ul class="nav navbar-nav">
+                    <li><a href="home.php"><span class="icon-home3"></span> Inicio</a></li>
+                    <li><a href="obras.php"><span class="icon-file-text"></span> Obras Publicas</a></li>
+                    <li><a href="estadisticas.php"><span class="icon-stats-dots"></span> Estadisticas</a></li>
+                    <li><a href="foro.php"><span class="icon-bubbles3"></span> Foro</a></li>
+                    <li><a href="login.php"><span class="icon-users"></span> Log In</a></li>
+                </ul>
+
+                <form action="" class="navbar-form navbar-right" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Buscar">
+                    </div>
+                    <button type="submit" class="btn btn-primary">
                         <span class="glyphicon glyphicon-search"></span>
                     </button>
-                    </form>
-                </div>
-
-                <div class="cajaRegistro">
-                    BIENVENIDO: 
-                    <?php
-                    // checamos si existe la session
-                    if (isset($_SESSION["id"])) {
-                        ?>
-                        <a href="perfil.php"><?php echo $_SESSION["nombre"]; ?></a> | 
-                        <?php
-                        if ($_SESSION["nombre"] == "admin") {
-                            ?>
-                            <a href="panel.php">PANEL</a> | <a href="usuarios.php">LISTADO DE USUARIOS</a> | 
-                            <?php
-                        }
-                        ?>
-                        <a href="salir.php">SALIR</a>
-                        <?php
-                    } else {
-                        ?><a href="Foro/registrarse.php">registrarse</a> - o - <a href="Foro/entrar.php">ingresar</a><?php
-                    }
-                    ?>
-                </div>
-
-                <div style="clear:both; height:1px;font-size:0px; line-height: 0px;"></div>
+                </form>
             </div>
-
-            <div class="cajaBanner">
-
-            </div>
-
-            <div class="cajaTitulo">
-                <h2><a href="<?php echo Conexion::ruta(); ?>">Crypto Change</a></h2>
-                <h4>Bienvenidos al foro de Crypto Change.</h4>
-            </div>
+        </div>
+    </nav>
+</header>
